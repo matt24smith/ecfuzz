fed94489c4add76f231cc8e9f2daaafbb3edd74e
matt24smith <matt24smith@gmail.com>
2023-01-20 13:56:59 -0400

    update example for changes to lib interface

--- examples/example_custom_fuzzer.rs
+++ examples/example_custom_fuzzer.rs
@@ -4,14 +4,12 @@ use std::io::{stdout, Write};
 use std::path::PathBuf;
 use std::time::Instant;
 
-use ecfuzz::corpus::{load_corpus, Corpus, CorpusInput};
-use ecfuzz::execute::{
-    check_report_coverage, count_branch_total, exec_target_args, index_target_report, Config, Exec,
-};
+use ecfuzz::corpus::{Corpus, CorpusInput};
+use ecfuzz::execute::{count_branch_total, Config, Exec, ExecResult};
 use ecfuzz::mutator::Mutation;
 
 #[repr(C)]
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct MyTargetInput {
     num1: c_uint,
     num2: c_uint,
@@ -57,6 +55,9 @@ impl MyTargetInput {
         CorpusInput {
             data: bytes,
             coverage,
+            lifetime: 0,
+            file_stem: PathBuf::from("MyCustomInput"),
+            file_ext: PathBuf::from("mutation"),
         }
     }
 
@@ -99,10 +100,16 @@ impl MyTargetInput {
 
 impl MyFuzzEngine {
     pub fn new() -> Self {
+        let multiplier = Some(0.01);
+        let dict_path: Option<PathBuf> = None;
+
+        let firstnames = Corpus::load_corpus_file(&PathBuf::from("examples/firstname.dict")).0;
+        let lastnames = Corpus::load_corpus_file(&PathBuf::from("examples/lastname.dict")).0;
+
         MyFuzzEngine {
-            mutation_engine: Mutation::new(None),
-            firstname_seeds: load_corpus(&PathBuf::from("examples/firstname.dict")),
-            lastname_seeds: load_corpus(&PathBuf::from("examples/lastname.dict")),
+            mutation_engine: Mutation::new(dict_path, multiplier),
+            firstname_seeds: firstnames,
+            lastname_seeds: lastnames,
             data: MyTargetInput {
                 num1: 0,
                 num2: 0,
@@ -114,20 +121,6 @@ impl MyFuzzEngine {
         }
     }
 
-    /// return mutated string arguments that will be sent to target argv
-    /// caution: data sent to the target program may contain invalid strings
-    pub fn get_target_args(&self) -> [String; 6] {
-        unsafe {
-            [
-                format!("{}", &self.data.num1).to_string(),
-                format!("{}", &self.data.num2).to_string(),
-                format!("{}", &self.data.num3).to_string(),
-                String::from_utf8_unchecked(self.data.str1.as_bytes().to_vec()),
-                String::from_utf8_unchecked(self.data.str2.as_bytes().to_vec()),
-                String::from_utf8_unchecked(self.data.str3.as_bytes().to_vec()),
-            ]
-        }
-    }
     fn mutate_number_1(mut self) -> Self {
         self.data.num1 = (self.mutation_engine.hashfunc() % 1000usize)
             .try_into()
@@ -214,14 +207,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     cfg.target_path = PathBuf::from("./examples/example.c");
     cfg.iterations = 10_000;
     cfg.objects = vec![PathBuf::from("./a.out")];
+    cfg.mutate_args = true;
 
     // compile target with instrumentation
     Exec::initialize(&cfg)?;
 
     // create new mutation engine and corpus, seed the corpus with zeros
     let mut engine = MyFuzzEngine::new();
+
+    println!("done initialize");
     let mut cov_corpus = Corpus::new();
     let mut crash_corpus = Corpus::new();
+
     let seed = MyTargetInput {
         num1: 0,
         num2: 0,
@@ -230,11 +227,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         str2: CString::from_vec_with_nul(b"\0".to_vec()).unwrap(),
         str3: CString::from_vec_with_nul(b"\0".to_vec()).unwrap(),
     };
-    cov_corpus.add(seed.serialize(HashSet::new()));
 
-    // initial profile paths
-    let rawprof = "init.profraw";
-    let profdata = "init.profdata";
+    println!("seeding: {:?}", &seed);
+    cov_corpus.add_and_distill_corpus(seed.serialize(HashSet::new()));
+
+    // coverage profile paths
+    let profraw = "output/custom1.profraw";
+    let profdata = "output/custom1.profdata";
 
     let mut out = stdout().lock();
 
@@ -246,47 +245,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // apply a mutation
         engine = engine.mutate(cov_corpus.total_coverage.len());
+        let mutated = engine.data.clone();
 
         // run the program with mutated inputs, log crashes to crash corpus
-        let args = engine.get_target_args();
-        let crashed = exec_target_args(rawprof, &args).unwrap();
-        if crashed {
-            crash_corpus
-                .inputs
-                .push(engine.data.serialize(input_raw.coverage));
-            continue;
-        }
+        let (entry, output) = Exec::trial(
+            &cfg,
+            profraw,
+            profdata,
+            &mutated.clone().serialize(HashSet::default()).data,
+            PathBuf::new(),
+            PathBuf::new(),
+            0,
+        );
 
-        // check the program coverage
-        index_target_report(&cfg, &rawprof, &profdata)?;
-        let coverage = check_report_coverage(&cfg, &profdata)?;
+        let output = match output {
+            ExecResult::Ok(o) => o,
+            ExecResult::Err(o) => o,
+        };
 
         // add inputs yielding new coverage to the corpus
-        if !cov_corpus.total_coverage.is_superset(&coverage) {
-            let corpus_entry = engine.data.serialize(coverage);
+        //if !cov_corpus.total_coverage.is_superset(&coverage) {
+        if output.stderr.is_empty() && !cov_corpus.total_coverage.is_superset(&entry.coverage) {
+            let corpus_entry = engine.data.serialize(entry.coverage);
             out.write_all(&[b'\r'])?;
             cov_corpus.add_and_distill_corpus(corpus_entry);
+            println!("\nnew entry! {}", cov_corpus);
+        } else if !output.stderr.is_empty() {
+            let corpus_entry = engine.data.serialize(entry.coverage);
+            out.write_all(&[b'\r'])?;
+            crash_corpus.add_and_distill_corpus(corpus_entry);
+            eprintln!("\ncrashed! {}", crash_corpus);
         }
 
         // log some status messages every 100 execs
         if i % cfg.iter_check == 0 && i > 0 {
             let branch_count = count_branch_total(&cfg, profdata)?;
             let status_msg = format!(
-                "\r{:>3} {:>3} {:>4}{}{:?}{}{:?}{}{:?}\t\tbranches: {:?}/{}\texec/s {:.2}\ti: {}",
-                &args[0],
-                &args[1],
-                &args[2],
-                vec![' '; 12 - args[3].len()].iter().collect::<String>(),
-                String::from_utf8_lossy(&args[3].as_bytes().to_vec()),
-                vec![' '; 12 - args[4].len()].iter().collect::<String>(),
-                String::from_utf8_lossy(&args[4].as_bytes().to_vec()),
-                vec![' '; 12 - args[5].len()].iter().collect::<String>(),
-                String::from_utf8_lossy(&args[5].as_bytes().to_vec()),
+                "\r{:0>3} {:0>3} {:0>4}  {: >10}  {: >10}\t{: >10}  branches: {}/{}  exec/s {:.2}  i: {}",
+                &mutated.num1,
+                &mutated.num2,
+                &mutated.num3,
+                String::from_utf8_lossy(&mutated.str1.as_bytes().to_vec()),
+                String::from_utf8_lossy(&mutated.str2.as_bytes().to_vec()),
+                String::from_utf8_lossy(&mutated.str3.as_bytes().to_vec()),
                 cov_corpus.total_coverage.len(),
                 branch_count,
                 cfg.iter_check as f32 / (timer_start.elapsed().as_millis() as f32 / 1000.0),
                 i
-            );
+                );
             timer_start = Instant::now();
             out.write_all(&status_msg.as_bytes().to_vec())?;
             out.flush()?;
@@ -294,8 +300,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // save the results
-    cov_corpus.save(&PathBuf::from("output/"), "example_output", false)?;
-    crash_corpus.save(&PathBuf::from("output/"), "example_crashes", false)?;
+    cov_corpus.save(PathBuf::from("output/example1/"))?;
+    crash_corpus.save(PathBuf::from("output/example1_crash/"))?;
 
     Ok(())
 }
